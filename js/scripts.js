ec63b425e39843cd258d5645a5cbd5a6a4ed81ac
Student <example@example.com>
2019-11-05 16:37:24 -0800

    add ability to get name and initial deposit created within JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,50 @@
 
-function BankAccount (name, initialDeposit) {
-  this.name = name,
-  this.initialDeposit = initialDeposit
+function BankAccount () {
+  this.name,
+  this.balance = 0
 }
 
-//user interface logic
-var bankAccount = new BankAccount ();
+//we were aiming for a prototype that takes the user input and sets properties of the object
+BankAccount.prototype.createAcct = function(name, initialDeposit) {
+  this.name = name;
+  this.balance = initialDeposit
+}
 
-$(document).ready(function() {
+//we were aiming for a prototype method that takes in a transaction and updates the inital deposit value
+BankAccount.prototype.newDeposit = function(initialDeposit, deposit) {
+  this.balance = initialDeposit + this.deposit
+}
+
+
+
+//user interface logic
+
+$(document).ready(function(){
   // attachContactListeners();
+  var bankAccount = new BankAccount();
   $("form#openAcct").submit(function(event) {
     event.preventDefault();
     var inputtedName = $("input#name").val();
     var inputtedInitialDeposit = parseInt($("input#initialDeposit").val());
-    console.log(inputtedInitialDeposit)
 
     $("input#name").val("");
     $("input#initialDeposit").val("");
-    //
-    $('.showInitialBalance').text(inputtedName +", your initial balance is " + inputtedInitialDeposit);
 
+    bankAccount.createAcct(inputtedName, inputtedInitialDeposit);
+    console.log(bankAccount)
   });
+
+
+  var bankAccount = new BankAccount();
+  $("form#existingAcct").submit(function(event){
+    event.preventDefault();
+    var inputtedDeposit = parseInt($("input#deposit").val());
+
+  $("input#deposit").val("");
+
+  bankAccount.newDeposit(inputtedDeposit);
+  // so far, this only returns NaN, so work on this form's entry isn't working
+  });
+
+
 });
